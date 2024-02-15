fbe673ed49e3ce129482cdd37a3a53aec02afd49
Karpov Dima AN-43 <karpov.dima1986@mail.ru>
2024-02-15 21:16:47 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,17 @@
+package ru.netology
+
+fun main() {
+    val amount = 20_000
+    val customer = true
+
+
+    val discount = if (amount in 1001..9999) {
+        amount - 100
+    } else (if (amount > 10_000) {
+        amount - (amount * 5 / 100)
+    } else amount)
+
+    val discountCustomer = if (customer) discount * 1 / 100 else discount
+
+    println("Стоимость покупки $discountCustomer руб.")
+}
